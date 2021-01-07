75c750379f67b8844d58f1da5c7b345e7f0f2729
krazykirby99999 <krazykirby99999@gmail.com>
2021-01-07 13:22:37 -0600

    Complete 5

--- app.py
+++ app.py
@@ -1,39 +1,69 @@
-
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 
 import sys
 
+class CustomDialog(QDialog):
+
+    def __init__(self, *args, **kwargs):
+        super(CustomDialog, self).__init__(*args, **kwargs)
+
+        self.setWindowTitle("HELLO!")
+
+        buttons = QDialogButtonBox.Ok | QDialogButtonBox.Cancel
+
+        self.buttonBox = QDialogButtonBox(buttons)
+        self.buttonBox.accepted.connect(self.accept)
+        self.buttonBox.rejected.connect(self.reject)
+
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.buttonBox)
+        self.setLayout(self.layout)
 
 class MainWindow(QMainWindow):
-
+    
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
 
         self.setWindowTitle("My App")
 
-        tabs = QTabWidget()
-        tabs.setDocumentMode(True)
-        tabs.setTabPosition(QTabWidget.East)
-        tabs.setMovable(True)
+        label = QLabel("This is great.")
+        label.setAlignment(Qt.AlignCenter)
 
-        for n, color in enumerate(['red', 'green', 'blue', 'yellow']):
-            tabs.addTab(Color(color),color)
-        
-        self.setCentralWidget(tabs)
+        self.setCentralWidget(label)
 
+        toolbar = QToolBar("Main toolbar")
+        toolbar.setIconSize(QSize(16,16))
+        self.addToolBar(toolbar)
 
-class Color(QWidget):
+        button_action = QAction(QIcon("bug.png"), "A Button", self)
+        button_action.setStatusTip("This is a button")
+        button_action.triggered.connect(self.onMyToolBarButtonClick)
+        button_action.setCheckable(True)
+        toolbar.addAction(button_action)
 
-    def __init__(self, color, *args, **kwargs):
-        super(Color, self).__init__(*args, **kwargs)
-        self.setAutoFillBackground(True)
+        toolbar.addSeparator()
 
-        palette = self.palette()
-        palette.setColor(QPalette.Window, QColor(color))
-        self.setPalette(palette)
+        button_action2 = QAction(QIcon("bug.png"), "A Button2", self)
+        button_action2.setStatusTip("This is a button2")
+        button_action2.triggered.connect(self.onMyToolBarButtonClick)
+        button_action2.setCheckable(True)
+        toolbar.addAction(button_action2)
 
+        toolbar.addWidget(QLabel("Hello"))
+        toolbar.addWidget(QCheckBox())
+
+        self.setStatusBar(QStatusBar(self))
+
+    def onMyToolBarButtonClick(self, s):
+        print("clicl", s)
+
+        dlg = CustomDialog(self)
+        if dlg.exec_():
+            print("Success!")
+        else:
+            print("Cancel!")
 
 app = QApplication(sys.argv)
 
