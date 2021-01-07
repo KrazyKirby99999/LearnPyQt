cf16c7e999b67ebdd68ebe1f1d5559e6126b8f3b
krazykirby99999 <krazykirby99999@gmail.com>
2021-01-07 11:50:00 -0600

    Complete 2

--- app.py
+++ app.py
@@ -16,6 +16,33 @@ class MainWindow(QMainWindow):
 
         self.setCentralWidget(label)
 
+        toolbar = QToolBar("Main toolbar")
+        toolbar.setIconSize(QSize(16,16))
+        self.addToolBar(toolbar)
+
+        button_action = QAction(QIcon("bug.png"), "A Button", self)
+        button_action.setStatusTip("This is a button")
+        button_action.triggered.connect(self.onMyToolBarButtonClick)
+        button_action.setCheckable(True)
+        toolbar.addAction(button_action)
+
+        toolbar.addSeparator()
+
+        button_action2 = QAction(QIcon("bug.png"), "A Button2", self)
+        button_action2.setStatusTip("This is a button2")
+        button_action2.triggered.connect(self.onMyToolBarButtonClick)
+        button_action2.setCheckable(True)
+        toolbar.addAction(button_action2)
+
+        toolbar.addWidget(QLabel("Hello"))
+        toolbar.addWidget(QCheckBox())
+
+        self.setStatusBar(QStatusBar(self))
+    
+
+    def onMyToolBarButtonClick(self, s):
+        print("click", s)
+
 app = QApplication(sys.argv)
 
 window = MainWindow()
