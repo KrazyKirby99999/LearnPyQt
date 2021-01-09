8e6837a12e43c12a7aca79a060eee1282a761f99
krazykirby99999 <krazykirby99999@gmail.com>
2021-01-09 10:16:42 -0600

    Complete 1

--- app.py
+++ app.py
@@ -1,73 +1,15 @@
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
-
 import sys
+from PyQt5 import QtWidgets, uic
 
-class CustomDialog(QDialog):
+from MainWindow import Ui_MainWindow
 
-    def __init__(self, *args, **kwargs):
-        super(CustomDialog, self).__init__(*args, **kwargs)
-
-        self.setWindowTitle("HELLO!")
-
-        buttons = QDialogButtonBox.Ok | QDialogButtonBox.Cancel
-
-        self.buttonBox = QDialogButtonBox(buttons)
-        self.buttonBox.accepted.connect(self.accept)
-        self.buttonBox.rejected.connect(self.reject)
-
-        self.layout = QVBoxLayout()
-        self.layout.addWidget(self.buttonBox)
-        self.setLayout(self.layout)
-
-class MainWindow(QMainWindow):
-    
-    def __init__(self, *args, **kwargs):
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self, *args, obj=None, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
+        self.setupUi(self)
 
-        self.setWindowTitle("My App")
-
-        label = QLabel("This is great.")
-        label.setAlignment(Qt.AlignCenter)
-
-        self.setCentralWidget(label)
-
-        toolbar = QToolBar("Main toolbar")
-        toolbar.setIconSize(QSize(16,16))
-        self.addToolBar(toolbar)
-
-        button_action = QAction(QIcon("bug.png"), "A Button", self)
-        button_action.setStatusTip("This is a button")
-        button_action.triggered.connect(self.onMyToolBarButtonClick)
-        button_action.setCheckable(True)
-        toolbar.addAction(button_action)
-
-        toolbar.addSeparator()
-
-        button_action2 = QAction(QIcon("bug.png"), "A Button2", self)
-        button_action2.setStatusTip("This is a button2")
-        button_action2.triggered.connect(self.onMyToolBarButtonClick)
-        button_action2.setCheckable(True)
-        toolbar.addAction(button_action2)
-
-        toolbar.addWidget(QLabel("Hello"))
-        toolbar.addWidget(QCheckBox())
-
-        self.setStatusBar(QStatusBar(self))
-
-    def onMyToolBarButtonClick(self, s):
-        print("clicl", s)
-
-        dlg = CustomDialog(self)
-        if dlg.exec_():
-            print("Success!")
-        else:
-            print("Cancel!")
-
-app = QApplication(sys.argv)
+app = QtWidgets.QApplication(sys.argv)
 
 window = MainWindow()
 window.show()
-
-app.exec_()
+app.exec()
